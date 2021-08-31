1e5db7a66a091d4deeeb3c456e65fc675843c58a
taki0412 <taki041210@naver.com>
2021-08-31 22:54:46 +0900

    [ADD] Device Set

--- datasets.py
+++ datasets.py
@@ -4,26 +4,43 @@
 """
 
 # Import Library
-import pandas as pd
-import numpy as np
-import cv2
-import os
-import PIL
-import random
-import glob
-import time
-import pickle
-from tqdm import tqdm
-from sklearn.model_selection import KFold
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
-import torchvision.transforms as T
-from torch.utils.data import DataLoader, Dataset
-from efficientnet_pytorch import EfficientNet
+import numpy as np
+import os, random
 
 # Set random seed
 seed = 42
 random.seed(seed)
 np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+os.environ["PYTHONHASGSEED"] = str(seed)
+print(f"seed : {seed}")
+
+# Set Path
+""" 해당 경로는 다음과 같은 구조를 가정합니다.
+data
+└── dirty_mnist
+     ├── model
+     └── data
+          ├── train_data
+          │   └── 00000~49999.png
+          └── test_data
+               └── 50000~54999.png
+"""
+
+os.chdir("./data/dirty_mnist")
+ROOT_PATH = os.getcwd()
+model_path = os.path.join(ROOT_PATH, 'model')
+train_data_path = os.path.join(ROOT_PATH, 'data/train_data')
+test_data_path = os.path.join(ROOT_PATH, 'data/test_data')
+
+print(f"ROOT_PATH : {ROOT_PATH}")
+print(f"model_path = {model_path}")
+print(f"train_data_path : {train_data_path}")
+print(f"test_data_path : {test_data_path}")
+
+# Set Device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f'device : {device}')
+print(torch.cuda.get_device_properties(device))
